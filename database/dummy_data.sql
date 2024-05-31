f1fc1a5b13472b9cd6184c716d682ff66a4678bd
wenddarkknight <wenddarkknight@gmail.com>
2024-05-31 10:15:33 +0000

    initial commit

---
+++ database/dummy_data.sql
@@ -0,0 +1,13 @@
+TRUNCATE TABLE tb_users;
+
+INSERT INTO tb_users (email, username, password) VALUES
+('user1@email.com', 'user1', 'password1'),
+('user2@email.com', 'user2', 'password2'),
+('user3@email.com', 'user3', 'password3'),
+('user4@email.com', 'user4', 'password4'),
+('user5@email.com', 'user5', 'password5'),
+('user6@email.com', 'user6', 'password6'),
+('user7@email.com', 'user7', 'password7'),
+('user8@email.com', 'user8', 'password8'),
+('user9@email.com', 'user9', 'password9'),
+('user10@email.com', 'user10', 'password10');
